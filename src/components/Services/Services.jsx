6fd2276a708a560a54267a25c9be51ba581d3c6f
Prince Singh <shiprincesingh@gmail.com>
2026-05-22 00:24:28 +0530

    ajdhfskj

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -58,7 +58,7 @@ const Services = () => {
       <div id="service" className="wrapper p-block-9 border-btn">
          <div className="flex between gap-4 flex-col items-center justify-center text-center">
           <div className="flex flex-col items-center justify-center text-center">
-            <span className=" text-6xl font-medium bg-linear-to-r from-orange-400 to-orange-600 bg-clip-text text-transparent overflow-hidden" datatype="Services">
+            <span className=" text-6xl font-medium bg-linear-to-r from-orange-400 to-orange-600 bg-clip-text text-transparent overlay-text  middle" datatype="Services">
               My Specialization
             </span>
 
